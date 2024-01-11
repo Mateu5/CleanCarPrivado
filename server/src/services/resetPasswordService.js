2b0fd62c85dd7e52b784ce1b7bb4044df4a1ed4a
MATEUS VINICIUS FERREIRA <mateus.ieq32@gmail.com>
2024-01-11 14:31:04 -0300

    att resetSenha

--- server/src/services/resetPasswordService.js
+++ server/src/services/resetPasswordService.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 const { conexao } = require('../banco-de-dados/connection');
 
+//cria um servico de tranporte
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -11,33 +12,36 @@ const transporter = nodemailer.createTransport({
         rejectUnauthorized: false // Desabilita a verificação do certificado SSL/TLS
     }
 })
-
+//funcao para reset da senha
 const resetSenha = async function (req, res) {
 
+    //recebe o email do front(json)
     const emailRec = req.body.email;
-
+    //verifica se o email esta no banco de dados 
     [ resultado, meta ] = await conexao.query("select * from usuarios where email = :email", {
         replacements: {
+            //substitui o :email pelo email que foi recebido do front
             email: emailRec
         }
     });
 
-    console.log(req.body.email);
-
+    console.log(emailRec);
+    //verica se o email não é nulo antes de gerar o código de verificação
     if(emailRec != null){
         console.log('entrou')
+        //cria um código de verificação aleatório com o Math do javaScript (precisa mudar a maneira de gerar o código)
         const codigoRecuperacao = Math.floor(Math.random() * 1000000);
-
+        //da um UPDATE no banco com o Código de verificação (futuramente precisa criar uma table propia no banco)
         [ resultado, meta ] = await conexao.query("UPDATE usuarios SET codigo_recuperacao = :codigo where email = :email", {
             replacements: {
                 codigo: codigoRecuperacao,
                 email: emailRec
             }
         });
-
+        
         const mailOptions = {
-            from: 'mateus.ieq32@gmail.com',
-            to: emailRec,
+            from: 'mateus.ieq32@gmail.com',//remetente
+            to: emailRec,//quem vai receber o código
             subject: 'Recuperação de Senha',
             text: `Seu código de recuperação de senha é: ${codigoRecuperacao}`
         }
